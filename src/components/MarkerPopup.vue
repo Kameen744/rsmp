6687643146f5f6780625214626bfb4f106687499
Kamal.A_SYDANI <kamalaminu94@gmail.com>
2024-05-22 16:07:35 +0100

    refine filters and add select all for program areas

--- src/components/MarkerPopup.vue
+++ src/components/MarkerPopup.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="z-[999] fixed bottom-3 right-3 grid grid-cols-4 gap-1 shadow bg-transparent">
+  <div class="z-[999] fixed bottom-3 right-3 flex shadow bg-transparent">
     <!-- <div class="grid col-span-3 bg-white p-2 max-h-[400px] overflow-y-auto c-scroll">
         <div class=" border-blue-600 border-solid border-2">
           <h1 class="text-2xl text-center font-bold" v-if="selectedMarker">{{ selectedMarker.lga }} LGA</h1>
@@ -44,35 +44,55 @@
             </tbody>
         </table>
     </div> -->
-  <div class="grid col-span-3 bg-white shadow-md p-2 max-h-[400px] w-[60vw] overflow-y-auto c-scroll">
-    <div class="border-blue-600 border-solid border-2">
-      <h1 class="text-2xl text-center font-bold" v-if="selectedMarker">{{ selectedMarker.lga }} LGA</h1>
-      <h1 class="text-2xl text-center font-bold" v-if="selectedLgaMarker">{{ selectedLgaMarker.LGA }} LGA</h1>
-    </div>
-    <div class="flex text-center bg-blue-500 text-white font-semibold">
-      <div class="py-2 px-2 min-w-[230px]">Support Type</div>
-      <div class="py-2 px-2 min-w-[300px]">Partner</div>
-      <div class="py-2 px-2 min-w-[110px]">Start Date</div>
-      <div class="py-2 px-2 min-w-[110px]">End Date</div>
-      <div class="py-2 px-2 min-w-[80px]">Status</div>
-    </div>
-    
-    <div class="flex flex-wrap rounded-none bg-blue-100 text-left hover:bg-blue-50 cursor-pointer" 
-      v-for="(val, key) in selectedLgaMarker.supports" :key="key" @click="expand(key)" :class="collapseKey==key?'bg-blue-50':'bg-blue-100'">
-      <div class="overflow-hidden border-y py-1 w-[230px] border-white px-3 flex justify-start">
-          <span class="inline-block w-3 h-3 m-0 rounded-full mr-1 mt-2" :style="`background: ${getSpBg(val.type_of_support)} `"></span>
-          {{ val.type_of_support }} 
+
+    <div class="grid bg-white shadow-md p-2 max-h-[400px] max-w-[60vw] overflow-y-auto c-scroll">
+      <div class="border-blue-600 border-solid border-2">
+        <h1 class="text-2xl text-center font-bold" v-if="selectedMarker">{{ selectedMarker.lga }} LGA</h1>
+        <h1 class="text-2xl text-center font-bold" v-if="selectedLgaMarker">{{ selectedLgaMarker.LGA }} LGA</h1>
       </div>
-      <div class="overflow-hidden border-y py-1 border-white px-3 w-[300px]">{{ val.partner }}</div>
-      <div class="overflow-hidden border-y py-1 border-white px-3 w-[110px]">{{ val.start_date }}</div>
-      <div class="overflow-hidden border-y py-1 border-white px-3 w-[110px]">{{ val.end_date }}</div>
-      <div class="overflow-hidden border-y py-1 border-white px-3 w-[80px]">{{ val.status }}</div>
-      <br>
-      <div class="flex-full py-2 px-4" v-if="collapseKey==key">
-        <h1>{{ val.partner }}</h1>
+      <div class="flex text-center bg-blue-500 text-white font-semibold">
+        <div class="py-2 px-2 min-w-[230px]">Support Type</div>
+        <div class="py-2 px-2 min-w-[300px]">Partner</div>
+        <div class="py-2 px-2 min-w-[110px]">Start Date</div>
+        <div class="py-2 px-2 min-w-[110px]">End Date</div>
+        <div class="py-2 px-2 min-w-[80px]">Status</div>
       </div>
+      
+      <template v-if="selectedMarker">
+        <div class="flex flex-wrap rounded-none bg-blue-100 text-left hover:bg-blue-50 cursor-pointer" 
+        :key="Math.round(Math.random() * 10001)" @click="expand(key)" :class="collapseKey==key?'bg-blue-50':'bg-blue-100'">
+          <div class="overflow-hidden border-y py-1 w-[230px] border-white px-3 flex justify-start">
+              <span class="inline-block w-3 h-3 m-0 rounded-full mr-1 mt-2" :style="`background: ${getSpBg(selectedMarker.type_of_support)} `"></span>
+              {{ selectedMarker.type_of_support }} 
+          </div>
+          <div class="overflow-hidden border-y py-1 border-white px-3 w-[300px]">{{ selectedMarker.partner }}</div>
+          <div class="overflow-hidden border-y py-1 border-white px-3 w-[110px]">{{ selectedMarker.start_date }}</div>
+          <div class="overflow-hidden border-y py-1 border-white px-3 w-[110px]">{{ selectedMarker.end_date }}</div>
+          <div class="overflow-hidden border-y py-1 border-white px-3 w-[80px]">{{ selectedMarker.status }}</div>
+          <br>
+          <div class="flex-full py-2 px-4" v-if="collapseKey==key">
+            <h1>{{ selectedMarker.partner }}</h1>
+          </div>
+        </div>
+      </template>
+      <template v-if="selectedLgaMarker">
+        <div class="flex flex-wrap rounded-none bg-blue-100 text-left hover:bg-blue-50 cursor-pointer" 
+        v-for="(val, key) in selectedLgaMarker.supports" :key="key" @click="expand(key)" :class="collapseKey==key?'bg-blue-50':'bg-blue-100'">
+        <div class="overflow-hidden border-y py-1 w-[230px] border-white px-3 flex justify-start">
+            <span class="inline-block w-3 h-3 m-0 rounded-full mr-1 mt-2" :style="`background: ${getSpBg(val.type_of_support)} `"></span>
+            {{ val.type_of_support }} 
+        </div>
+        <div class="overflow-hidden border-y py-1 border-white px-3 w-[300px]">{{ val.partner }}</div>
+        <div class="overflow-hidden border-y py-1 border-white px-3 w-[110px]">{{ val.start_date }}</div>
+        <div class="overflow-hidden border-y py-1 border-white px-3 w-[110px]">{{ val.end_date }}</div>
+        <div class="overflow-hidden border-y py-1 border-white px-3 w-[80px]">{{ val.status }}</div>
+        <br>
+        <div class="flex-full py-2 px-4" v-if="collapseKey==key">
+          <h1>{{ val.partner }}</h1>
+        </div>
+        </div>
+      </template>
     </div>
-  </div>
     <div class=" bg-white px-2 max-h-[400px] overflow-hidden">
       <b @click="store.closePopup" class="cursor-pointer absolute top-1 right-1 ml-4 w-8 h-8 rounded-full bg-blue-200 text-center text-xs text-blue-900">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="mt-1 ml-1 w-6 h-6">
@@ -95,6 +115,7 @@
         </template> 
       </ul>
     </div>
+  
   </div>
 </template>
 <script setup>
